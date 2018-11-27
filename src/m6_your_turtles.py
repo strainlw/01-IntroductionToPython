1957376bbbb84624a04090f70ec694ca052fc1f6
strainlw <strainlw@rose-hulman.edu>
2018-11-27 17:57:03 -0500

    Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Loki Strain.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,30 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window=rg.TurtleWindow()
+
+yaboi=rg.SimpleTurtle('turtle')
+yagirl=rg.SimpleTurtle('turtle')
+yaboi.pen=rg.Pen('blue',5)
+yagirl.pen=rg.Pen('purple',5)
+yaboi.speed=10
+yagirl.speed=10
+yaboi.pen_up()
+yagirl.pen_up()
+yaboi.right(90)
+yaboi.forward(300)
+yagirl.right(90)
+yagirl.forward(300)
+yaboi.left(90)
+yagirl.left(90)
+yaboi.pen_down()
+yagirl.pen_down()
+
+for k in range(7):
+    size=(50*(k+1))
+    yaboi.backward(size)
+    yaboi.draw_square(size*2)
+    yaboi.forward(size)
+    yagirl.draw_circle(size)
+

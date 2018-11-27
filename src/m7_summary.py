e2b0fd9cd925043f9997c100ec177cfe62ea9860
strainlw <strainlw@rose-hulman.edu>
2018-11-27 18:30:33 -0500

    Done

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Loki Strain.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -43,3 +43,18 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+window=rg.TurtleWindow()
+blueboi=rg.SimpleTurtle('turtle')
+blueboi.pen=rg.Pen('blue',10)
+blueboi.left(90)
+blueboi.forward(200)
+blueboi.pen_up()
+blueboi.go_to(rg.Point(100,-40))
+blueboi.pen_down()
+blueboi.pen=rg.Pen('green',10)
+blueboi.backward(150)
+window.close_on_mouse_click()
+
+
